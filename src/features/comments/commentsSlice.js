3742b5b0ad1c91194261200cc0bdca1688014c15
Jordan Piper <jordanannpiper@gmail.com>
2022-12-29 08:21:58 -0800

    first commit

---
+++ src/features/comments/commentsSlice.js
@@ -0,0 +1,29 @@
+import { createSlice } from '@reduxjs/toolkit';
+import { COMMENTS } from '../../app/shared/COMMENTS';
+
+const initialState = {
+    commentsArray: COMMENTS
+};
+
+const commentsSlice = createSlice({
+    name: 'comment',
+    initialState,
+    reducers: {
+        addComment: (state, action) => {
+            const newComment = {
+                id: state.commentsArray.lenght + 1,
+                ...action.payload
+            };
+            state.commentsArray.push(newComment);
+        }
+    }
+});
+
+export const commentsReducer = commentsSlice.reducer;
+
+export const { addComment } = commentsSlice.actions;
+
+export const selectCommentsByCampsiteId = (campsiteId) => (state) => {
+    return state.comments.commentsArray.filter(
+        (comment) => comment.campsiteId === parseInt(campsiteId));
+}
